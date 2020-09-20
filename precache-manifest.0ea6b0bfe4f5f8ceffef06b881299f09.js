c2827bfe0570e484aade172db691230606fcae2c
Michael Hoha <michael1hoha@gmail.com>
2020-09-20 16:25:52 +0300

    Updates

---
+++ precache-manifest.0ea6b0bfe4f5f8ceffef06b881299f09.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74486511889491d4dbdf1990afeae548",
+    "url": "/react-mission/index.html"
+  },
+  {
+    "revision": "b2e2a9e8275e8981a149",
+    "url": "/react-mission/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "2579865caee6f5d3f13c",
+    "url": "/react-mission/static/js/2.435276df.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-mission/static/js/2.435276df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b2e2a9e8275e8981a149",
+    "url": "/react-mission/static/js/main.fc27ca70.chunk.js"
+  },
+  {
+    "revision": "ce2e5362dedfb8427c06",
+    "url": "/react-mission/static/js/runtime-main.331e45da.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-mission/static/media/logo.5d5d9eef.svg"
+  }
+]);
