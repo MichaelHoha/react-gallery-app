133fe5c36c86f5a8ce10e554d007e15ffa8e4412
Michael Hoha <michael1hoha@gmail.com>
2020-09-20 20:32:23 +0300

    new changes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,51 @@
 import React from 'react';
-import logo from './logo.svg';
+import ImageData from './data/img.json';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>check number 1!</h1>
+      <div className='header'>
+        <button className='left-button'>next</button>
+        <button className='right-button'>back</button>
+        <h1>gallery</h1>
+
+      </div>
+
+      <div className='gallery'>
+        {ImageData.map((element, index) => {
+          return (
+            <img className='gallery-pics' src={element.url} alt='react-task' />
+          )
+        })}
+
+      </div>
+
+      <div className='conten'>
+        <p>content</p>
+
+      </div>
+
+      <div className='lorem-ipsum'>
+        <p>Lorem ipsum dolor sit amet,consectetur adipiscing elit,
+        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+        Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium,
+        totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.
+        Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.
+        Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam,
+        quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur?
+        Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur,
+        vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?</p>
+
+      </div>
+
+      <div className='footer'>
+        <p>Michael Hohashvili.</p>
+
+      </div>
+
     </div>
   );
 }
